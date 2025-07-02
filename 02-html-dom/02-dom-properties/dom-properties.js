1cc97568d4b55fbabaafb220975f6a6e10d55511
lsindhu <sindhu151095@gmail.com>
2025-07-02 12:38:44 +0530

    HTML DOM Basics - Methods & Properties

---
+++ 02-html-dom/02-dom-properties/dom-properties.js
@@ -0,0 +1,23 @@
+// ✅ DOM Properties
+
+// Once you've selected an element, you can use **DOM properties**
+// to read or change its content and structure.
+
+// some important DOM properties are :
+
+// 📌 innerText
+// Gets or sets only the visible text of the element (ignores hidden elements)
+
+//change the text content inside ul to "I am setting something"
+
+// 📌 innerHTML
+// Gets or sets the full HTML content inside the element (can include tags)
+
+
+
+// 📌 textContent
+// Gets or sets all text content inside the element, including hidden text
+
+
+
+
---
+++ 02-html-dom/objects.js
@@ -0,0 +1,38 @@
+/* const person = {
+    name : 'sindhu',
+    college : 'GDC',
+    tasks : ['eat','sleep','teach'],
+    address : {
+        permanent : 'hyderbad',
+        current : 'Bangalore'
+    },
+    greetUser : function() {
+        console.log('say hello');
+    }
+}
+
+console.log(person.college);
+console.log(person.address);
+(person.greetUser()); */
+
+
+const window = {
+    alert : 'alert something',
+    console : {
+        log : 'log something',
+        dir : 'represent any node as js object'
+    },
+    document: {
+        documentElemenet : {
+            head : {
+                meta : 'meta information',
+                title : 'title'
+            },
+            body : {
+                h1 : 'heading',
+                p : 'paragraph',
+                button : 'button'
+            }
+        }
+    }
+}
